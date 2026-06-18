025cdf0818ca818329626412aa168c6cc6eda40e
Frince Nacion <louienacion9@gmail.com>
2026-06-18 11:16:37 +0800

    Refactor: simplify test function to directly print parsed functions

--- backend/tests/test_python_parser.py
+++ backend/tests/test_python_parser.py
@@ -1,10 +1,8 @@
 from analysis.parser_python import parse_python_file
  
 def test():
-    functions = parse_python_file(file_path='backend/tests/dummy_functions.py')
+    parsed = parse_python_file(file_path='backend/tests/dummy_functions.py')
 
-    print('line no# | function name')
-    for line_number, function in functions.items():
-        print(line_number, '       ', function[0])
-
+    print(parsed['functions'])
+    
 test()
