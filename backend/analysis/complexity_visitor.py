c54d429397642b3fe9b6c05053b4f646f0045649
Frince Nacion <louienacion9@gmail.com>
2026-06-18 16:52:04 +0800

    Update: enhance complexity tracking

--- backend/analysis/complexity_visitor.py
+++ backend/analysis/complexity_visitor.py
@@ -2,52 +2,86 @@ import ast
 
 class ComplexityVisitor(ast.NodeVisitor):
     def __init__(self):
-        super().__init__()
-        self.functions = []
+        self.complexity = 1 
         self.current_depth = 0
         self.max_depth = 0
-        
+
+        self.functions = []
+
     def enter_block(self):
         self.current_depth += 1
         self.max_depth = max(self.max_depth, self.current_depth)
 
     def exit_block(self):
         self.current_depth -= 1
-        
+
     def visit_If(self, node):
+        self.complexity += 1
+
         self.enter_block()
         self.generic_visit(node)
         self.exit_block()
 
     def visit_For(self, node):
+        self.complexity += 1
+
         self.enter_block()
         self.generic_visit(node)
         self.exit_block()
 
     def visit_While(self, node):
+        self.complexity += 1
+
         self.enter_block()
         self.generic_visit(node)
         self.exit_block()
-    
+
     def visit_Try(self, node):
+        self.complexity += len(node.handlers)
+
         self.enter_block()
         self.generic_visit(node)
         self.exit_block()
 
     def visit_Match(self, node):
+        self.complexity += len(node.cases)
+
         self.enter_block()
         self.generic_visit(node)
         self.exit_block()
 
-    def visit_FunctionDef(self, node):
-        function_visitor = ComplexityVisitor()
+    def visit_ListComp(self, node):
+        self.complexity += 1
+        self.generic_visit(node)
 
-        for statement in node.body:
-            function_visitor.visit(statement)
+    def visit_SetComp(self, node):
+        self.complexity += 1
+        self.generic_visit(node)
+
+    def visit_DictComp(self, node):
+        self.complexity += 1
+        self.generic_visit(node)
+
+    def visit_GeneratorExp(self, node):
+        self.complexity += 1
+        self.generic_visit(node)
+
+    def visit_BoolOp(self, node):
+        # if a and b and c
+        self.complexity += len(node.values) - 1
+        self.generic_visit(node)
+
+    def visit_FunctionDef(self, node):
+        func_visitor = ComplexityVisitor()
+
+        for stmt in node.body:
+            func_visitor.visit(stmt)
 
         self.functions.append({
-            'name' : node.name,
-            'line' : node.lineno
+            "name": node.name,
+            "complexity": func_visitor.complexity,
+            "max_depth": func_visitor.max_depth,
+            "line": node.lineno
         })
-        
+
         self.generic_visit(node)
