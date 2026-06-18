a08c00b35f47b730978917dd0dac341faf21aa20
Frince Nacion <louienacion9@gmail.com>
2026-06-18 13:52:52 +0800

    Update: added block depth tracking methods

--- backend/analysis/complexity_visitor.py
+++ backend/analysis/complexity_visitor.py
@@ -4,6 +4,40 @@ class ComplexityVisitor(ast.NodeVisitor):
     def __init__(self):
         super().__init__()
         self.functions = []
+        self.current_depth = 0
+        self.max_depth = 0
+        
+    def enter_block(self):
+        self.current_depth += 1
+        self.max_depth = max(self.max_depth, self.current_depth)
+
+    def exit_block(self):
+        self.current_depth -= 1
+        
+    def visit_If(self, node):
+        self.enter_block()
+        self.generic_visit(node)
+        self.exit_block()
+
+    def visit_For(self, node):
+        self.enter_block()
+        self.generic_visit(node)
+        self.exit_block()
+
+    def visit_While(self, node):
+        self.enter_block()
+        self.generic_visit(node)
+        self.exit_block()
+    
+    def visit_Try(self, node):
+        self.enter_block()
+        self.generic_visit(node)
+        self.exit_block()
+
+    def visit_Match(self, node):
+        self.enter_block()
+        self.generic_visit(node)
+        self.exit_block()
 
     def visit_FunctionDef(self, node):
         function_visitor = ComplexityVisitor()
