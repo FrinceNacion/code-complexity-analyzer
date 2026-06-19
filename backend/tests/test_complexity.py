592a294126011c29ffb4e92b88e5c4591b99dc4e
Frince Nacion <louienacion9@gmail.com>
2026-06-19 13:01:24 +0800

    Add: initial test for parsing Python files in complexity analysis

---
+++ backend/tests/test_complexity.py
@@ -0,0 +1,10 @@
+from analysis.parser_python import parse_python_file
+
+def test():
+    parsed = parse_python_file(file_path='backend/tests/dummies/complexity.py')
+
+    assert len(parsed) > 0
+
+    print(parsed) 
+
+test()
