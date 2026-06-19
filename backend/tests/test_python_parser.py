cd4ba05f4dcaf20c880ad8956c33a53212f4ad1a
Frince Nacion <louienacion9@gmail.com>
2026-06-19 08:47:14 +0800

    Update: fix file path in test and output formatting

--- backend/tests/test_python_parser.py
+++ backend/tests/test_python_parser.py
@@ -1,8 +1,15 @@
 from analysis.parser_python import parse_python_file
  
 def test():
-    parsed = parse_python_file(file_path='backend/tests/dummy_functions.py')
+    parsed = parse_python_file(file_path='backend/tests/dummies/functions.py')
 
     print(parsed['functions'])
+
+    for function in parsed['functions']:
+        print(f'Name: {function['name']}')
+        print(f'---- Line number: {function['line']}')
+        print(f'---- Complexity: {function['complexity']}')
+        print(f'---- Max depth: {function['max_depth']}\n')
+        
     
 test()
