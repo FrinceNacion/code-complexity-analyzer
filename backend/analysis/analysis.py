8a92d3b6c4aaa1ba3583b45251305ad0040ee280
Frince Nacion <louienacion9@gmail.com>
2026-06-25 21:39:56 +0800

    Update: added path validation

--- backend/analysis/analysis.py
+++ backend/analysis/analysis.py
@@ -3,10 +3,19 @@ import os
 def analyze():
     path = input('Enter python file path: ')
 
-    if not os.path.exists(path):
-        print('file does not exist')
+    if not os.path.isabs(path):
+        path = os.path.abspath(path)
+        print(path)
 
-    pass
+    if not os.path.exists(path):
+        print('path does not exist')
+        return
+    
+    if not os.path.isfile(path):
+        print('path not file')
+        return
+
+    print('passed')
     
 if __name__ == '__main__':
     analyze()
