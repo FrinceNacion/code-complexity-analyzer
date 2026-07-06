80f933aeb7bde5ca22eb5271d0f8207e3082debc
Frince Nacion <louienacion9@gmail.com>
2026-07-06 13:07:25 +0800

    Add: implement useAnalysis hook

---
+++ frontend/src/hooks/useAnalysis.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:8000"
+
+export  function useAnalysis() {
+    const [result, setResult] = useState(null);
+    const [error, setError] = useState(null);
+
+    async function analyze(fileInfo) {
+        const form = new FormData();
+        form.append("file", fileInfo.file);
+
+        try{
+            const response = await fetch(`${API_URL}/api/analyze`, {
+                method: "POST",
+                contentType: "multipart/form-data",
+                body: form,
+            });
+
+            if (!response.ok) {
+                const error_date = await response.json();
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            setResult(data);
+        } catch (error) {
+            setError(error.message);
+        }
+    }
+    return { result, error, analyze };
+}
