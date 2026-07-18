ee3bfdabef6e88e3e5779c3ed3124f1f62ab1d4d
Frince Nacion <louienacion9@gmail.com>
2026-07-18 12:38:58 +0800

    Update: enhance function name and added initial detectLanguageFromString function using highlight.js

--- frontend/src/components/utils/language_detector.js
+++ frontend/src/components/utils/language_detector.js
@@ -1,9 +1,16 @@
+import hljs from "highlight.js";
 
-export function detectLanguage(filename) {
+export function detectLangaugeFromFile(filename) {
     if (!filename) return "plaintext";
     const ext = filename.split(".").pop().toLowerCase();
     if (ext === "py") return "python";
     if (ext === "js") return "javascript";
     if (ext === "ts") return "typescript";
     return "plaintext";
+}
+
+export async function detectLanguageFromString(raw_code) {
+    const result = hljs.highlightAuto(raw_code) 
+    console.log(result.language)
+    return result.language
 }
